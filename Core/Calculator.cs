ccca9736983e2f3afba5746261c8ab860af3a98e
runarer <95697611+runarer@users.noreply.github.com>
2025-12-25 15:37:34 +0100

    Implements interface for double methods

--- Core/Calculator.cs
+++ Core/Calculator.cs
@@ -7,17 +7,37 @@ public class Calculator
         throw new NotImplementedException();
     }
 
+    public double Add(double numberA, double numberB)
+    {
+        throw new NotImplementedException();
+    }
+
     public int Sub(int numberA, int numberB)
     {
         throw new NotImplementedException();
     }
+
+    public double Sub(double numberA, double numberB)
+    {
+        throw new NotImplementedException();
+    }    
 
     public int Multi(int numberA, int numberB)
     {
         throw new NotImplementedException();
     }
 
+    public double Multi(double numberA, double numberB)
+    {
+        throw new NotImplementedException();
+    }    
+
     public int Div(int numberA, int numberB)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Div(double numberA, double numberB)
     {
         throw new NotImplementedException();
     }
