9648f1fa83ac8f8146a1b7e291e47a001af38ab7
runarer <95697611+runarer@users.noreply.github.com>
2025-12-25 15:35:15 +0100

    Implements Interface for ints mothods

---
+++ Core/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Core;
+
+public class Calculator
+{
+    public int Add(int numberA, int numberB)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Sub(int numberA, int numberB)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Multi(int numberA, int numberB)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Div(int numberA, int numberB)
+    {
+        throw new NotImplementedException();
+    }
+}
