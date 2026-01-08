bf89f0418f32224239afc288867ae59f8a1b3f16
runarer <95697611+runarer@users.noreply.github.com>
2026-01-08 12:20:15 +0100

    Basic menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿
+﻿using Spectre.Console;
 
+string[] choices = ["Addition", "Subtraction", "Multiplication","Division","Exit"];
+
+while (true) {
+    string choice = AnsiConsole.Prompt( new SelectionPrompt<string>()
+        .Title("Please select action (up and down, enter to select)")
+        .AddChoices(choices)
+    );
+
+    if(choice == choices[^1])
+        break;
+
+
+}
